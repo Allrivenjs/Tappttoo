a75fb4e3c8b07b215dc0b8914539ab6fc80fa8cd
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2022-12-02 11:03:02 -0500

    Update TattooartistFactory.php

--- database/factories/TattooartistFactory.php
+++ database/factories/TattooartistFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\tattoo_artist>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tattoo_artist>
  */
-class TattooArtistFactory extends Factory
+class TattooartistFactory extends Factory
 {
     /**
      * Define the model's default state.
