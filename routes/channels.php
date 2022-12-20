c83eefc64214192c95aebcb36880c422620db149
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2022-12-20 13:18:42 -0500

    up

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Broadcasting\ChatChannel;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -12,7 +13,12 @@ use Illuminate\Support\Facades\Broadcast;
 | used to check if an authenticated user can listen to the channel.
 |
 */
-
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('chat-channel.{room_id}', ChatChannel::class);
+
+Broadcast::channel('channel-session', function ($user) {
+    return $user;
 });
