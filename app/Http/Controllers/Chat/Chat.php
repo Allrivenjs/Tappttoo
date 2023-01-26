4b67da4ce89a5a4e43d98bff42f4e6636ca490c2
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-01-26 15:52:17 -0500

    Update Chat.php

--- app/Http/Controllers/Chat/Chat.php
+++ app/Http/Controllers/Chat/Chat.php
@@ -60,7 +60,12 @@ class Chat implements ChatInterface
             'rooms'=> [
                 'users' => fn ($q) => $q->where('users.id', '!=', Auth::guard('api')->user()->getAuthIdentifier()),
             ]
-        ])->find(Auth::guard('api')->user()->getAuthIdentifier());
+        ])->find(Auth::guard('api')->user()->getAuthIdentifier())->map(function ($room) {
+            return [
+                ...$room->rooms,
+                'last_message' => $room->rooms->lastMessage->first(),
+            ];
+        });
         return $rooms;
     }
 
