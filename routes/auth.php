9af303a1acdd85b53e8d34be3b1c1f37894700bb
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-01-27 18:08:29 -0500

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -8,13 +8,13 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest')
-                ->name('register');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest')
-                ->name('login');
+//Route::post('/register', [RegisteredUserController::class, 'store'])
+//                ->middleware('guest')
+//                ->name('register');
+//
+//Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+//                ->middleware('guest')
+//                ->name('login');
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
                 ->middleware('guest')
@@ -25,13 +25,13 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->middleware(['signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware(['auth', 'throttle:6,1'])
+                ->middleware(['throttle:6,1'])
                 ->name('verification.send');
 
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
-                ->name('logout');
+//Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+//                ->middleware('auth')
+//                ->name('logout');
