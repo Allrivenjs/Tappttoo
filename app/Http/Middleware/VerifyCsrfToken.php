22ddb91b5ac0fc3ffc5c180dcc1acc52621c33f7
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-01-27 18:20:37 -0500

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,9 +13,9 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/api/*',
-        '/forgot-password',
-        '/reset-password',
-        '/verify-email/*',
-        '/email/verification-notification'
+//        '/forgot-password',
+//        '/reset-password',
+//        '/verify-email/*',
+//        '/email/verification-notification'
     ];
 }
