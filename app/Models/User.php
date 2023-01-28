63d4ac5c8ffea7c5b7af0032104bedaa1841f0c9
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-01-27 19:06:29 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Overtrue\LaravelFollow\Traits\{Followable, Follower};
-use Laravel\Sanctum\HasApiTokens;
+
+use Laravel\Passport\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
