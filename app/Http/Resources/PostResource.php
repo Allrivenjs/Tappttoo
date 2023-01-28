043760280b4646b982e951e9e95c68ca79aa5514
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-01-28 15:12:49 -0500

    up

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,7 +14,7 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        $userAuth = auth()->guard('web')?->user();
+        $userAuth = auth()->guard('api')?->user();
         return array_merge(parent::toArray($request), [
             'likedByMe'=> $userAuth ? $this->liked($userAuth->getAuthIdentifier()) : false,
             'roles'=> $this->user->getRoleNames(),
