5a1b171a520fa58950447d499e22696b9b959e32
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-02-02 08:28:15 -0500

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,8 +39,8 @@ return [
                 'host' => env('APP_URL'),
                 'port' => 6001,
                 'scheme' => env('PUSHER_SCHEME', 'http'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'http') === 'https',
+//                'encrypted' => true,
+//                'useTLS' => env('PUSHER_SCHEME', 'http') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
