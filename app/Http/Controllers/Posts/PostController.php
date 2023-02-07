828691a7ba0d2997376cec8ac1813f3bfdeb4494
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-02-07 11:55:41 -0500

    Update PostController.php

--- app/Http/Controllers/Posts/PostController.php
+++ app/Http/Controllers/Posts/PostController.php
@@ -43,7 +43,7 @@ class PostController extends Controller
                     'comments_lasted'=> [ 'replies', 'owner' ]
                 ])->whereHas('topics', function (Builder $query) {
                     $mypreferences = $this->authApi()->user()?->preferences()->pluck('name')->toArray();
-                    $ramdomPreferens = Topic::all()->whereNotIn('name', $mypreferences)->random(2)->pluck('name')->toArray();
+                    $ramdomPreferens = Topic::all()->whereNotIn('name', $mypreferences ?? [] )->random(2)->pluck('name')->toArray();
                     $query->whereIn('name', array_merge($mypreferences ?? [], $ramdomPreferens));
                 })->orderByDesc('created_at')->simplePaginate(10)
         ))->response();
