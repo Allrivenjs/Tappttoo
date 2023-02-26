9e5b5038882c1564e77222cff448e98ce7be3542
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2023-02-26 15:49:15 -0500

    Update FileTrait.php

--- app/Traits/FileTrait.php
+++ app/Traits/FileTrait.php
@@ -40,7 +40,7 @@ trait FileTrait
         $storage = Storage::disk($type);
         abort_if(! $storage->exists($path), Response::HTTP_NOT_FOUND, 'File not found');
 
-        $this->file = $response ? ($storage->response($path)) : ($storage->get(($path)));
+        $this->file = $response ? ($storage->response($path)) : ($storage->readStream($path));
     }
 
     /**
